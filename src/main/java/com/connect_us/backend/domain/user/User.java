bfc31c40b7bd09b245b17943371bcf4d54223aa4
hanbi <rain7i@naver.com>
2020-08-21 20:54:26 +0900

    [MODIFY] modify user, delete views

--- src/main/java/com/connect_us/backend/domain/user/User.java
+++ src/main/java/com/connect_us/backend/domain/user/User.java
@@ -1,31 +1,31 @@
 package com.connect_us.backend.domain.user;
 
 import com.connect_us.backend.domain.BaseTimeEntity;
+import com.connect_us.backend.domain.cart.Cart;
 import com.connect_us.backend.domain.enums.Gender;
 import com.connect_us.backend.domain.enums.Role;
 import com.connect_us.backend.domain.enums.Social;
 import com.connect_us.backend.domain.enums.UserStatus;
 import com.connect_us.backend.domain.order.Order;
-import com.connect_us.backend.domain.cart.Cart;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 //@Column: not necessary if the field has no specific condition
 
-@Getter
-@NoArgsConstructor
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class User extends BaseTimeEntity {
+public class User extends BaseTimeEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="user_id")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)//같은 이메일 찾기
     private String email;
 
     private String password;
@@ -82,4 +82,51 @@ public class User extends BaseTimeEntity {
         return this.role.getKey();
     }
 
+    /*UserDetails 상속, db에서 유저정보 가져와서 담음*/
+
+    //권한 Collection 형태로 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> roles = new HashSet<>();
+        for(Role r : Role.values()){ //enum list로 가져옴
+            roles.add(new SimpleGrantedAuthority(r.name()));
+        }
+        return roles;
+    }
+
+    //사용자 email 반환
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    //사용자 pw 반환
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    //계정 만료 여부 반환, true: 만료되지 않음
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //pw 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    //계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/connect_us/backend/domain/user/UserRepository.java
+++ src/main/java/com/connect_us/backend/domain/user/UserRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    //JpaRepository 상속 -> 기본 CRUD 만들 필요 없음
+    /*
+    ** JpaRepository 상속 -> 기본 CRUD 만들 필요 없음 **
+    *  save(): 레코드 저장 (insert, update)
+    *  findOne(): primary key로 레코드 한건 찾기
+    *  findAll(): 전체 레코드 불러오기. 정렬(sort), 페이징(pageable) 가능
+    *  count():	레코드 갯수
+    *  delete(): 레코드 삭제
+    * */
     Optional<User> findByEmail(String email); //이미 가입한 회원인지 확인
+
 }
