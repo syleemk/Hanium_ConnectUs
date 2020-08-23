4f1bf952cdf882e0e572e77e5725662e2b4961f2
hanbi <rain7i@naver.com>
2020-08-23 17:09:18 +0900

    [ADD] Spring Security - join, login, role

---
+++ src/main/java/com/connect_us/backend/security/SecurityConfig.java
@@ -0,0 +1,94 @@
+package com.connect_us.backend.security;
+
+import com.connect_us.backend.domain.enums.Role;
+import com.connect_us.backend.security.handler.CustomAuthenticationFailureHandler;
+import com.connect_us.backend.security.handler.CustomAuthenticationHandler;
+import com.connect_us.backend.security.provider.CustomAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity //Spring Security 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+
+    @Bean //Security에서 제공하는 비밀번호 암호화 객체
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //static file 무시
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    //http 관련 인증
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/","/oauth2/**","/h2-console/**", "/v1/auth/**", "/v1/auth/login*").permitAll()
+                    .antMatchers("/v1/admin/**").hasRole(Role.ADMIN.name()) //관리자페이지 권한
+                    .antMatchers("/v1/seller/**").hasRole(Role.SELLER.name())//판매자페이지 권한
+                    .antMatchers("/v1/user/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .headers().frameOptions().disable()
+                .and()
+                    .formLogin()
+                    .loginPage("/v1/auth/login")
+                    .loginProcessingUrl("/v1/auth/login") //로그인 form 의 action 과 일치시켜주어야 한다.
+                    .usernameParameter("email")
+                    .defaultSuccessUrl("/v1/home")
+                    .successHandler(new CustomAuthenticationHandler())
+                    .failureHandler(new CustomAuthenticationFailureHandler())
+                    .permitAll()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/") // 로그아웃 성공시 home으로
+                        .invalidateHttpSession(true);
+//                .and()
+//                    .authenticationProvider(AuthProvider);
+//                .and()
+//                    .oauth2Login()
+//                    .userInfoEndpoint()//로그인 성공 후 사용자 정보 가져올때 설정 담당
+//                    .userService(customOAuth2UserService);//로그인 성공시 후속 조치 진행
+
+    }
+
+    //로그인 인증
+    //관리자 계정, 판매자 계정 inmemory에 생성
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        String password = passwordEncoder().encode("1234");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("seller").password(password).roles("SELLER");
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new CustomAuthenticationProvider();
+    }
+
+
+}
---
+++ src/main/java/com/connect_us/backend/security/handler/CustomAuthenticationHandler.java
@@ -0,0 +1,41 @@
+package com.connect_us.backend.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationHandler extends SimpleUrlAuthenticationSuccessHandler {
+    // 이전 사용자의 요청을 담고있는 객체
+    private RequestCache requestCache = new HttpSessionRequestCache();
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    // 인증 성공 이후에 request, response 이용해 다양한 기능추가 가능
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        //사용자가 인증에 성공하기 전의 정보 꺼냄
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        setDefaultTargetUrl("/");
+
+        //로그인 인증 시도시 정보 없는 경우에는 null
+        if(savedRequest!=null){
+            String targetUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request,response,targetUrl);
+        }else{//default page
+            redirectStrategy.sendRedirect(request, response, getDefaultTargetUrl());
+        }
+    }
+}
+
