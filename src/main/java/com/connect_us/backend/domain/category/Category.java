09c1a4cd0a42428c637a4723bb848884513c3d8d
guswns <guswns3371@gmail.com>
2020-08-25 17:15:17 +0900

    [REFACTORING] Category 엔티티 수정

--- src/main/java/com/connect_us/backend/domain/category/Category.java
+++ src/main/java/com/connect_us/backend/domain/category/Category.java
@@ -1,5 +1,6 @@
 package com.connect_us.backend.domain.category;
 
+import com.connect_us.backend.domain.BaseEntity;
 import com.connect_us.backend.domain.product.Product;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,12 +13,15 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Category {
+public class Category extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     private Long id;
+
+    @Column(name = "name")
+    private String name;
 
     @OneToMany(mappedBy = "category")
     List<Product> Products = new ArrayList<Product>();
