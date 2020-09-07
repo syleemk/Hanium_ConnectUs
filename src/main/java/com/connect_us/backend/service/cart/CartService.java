3298a36ff6b2b315a5156eec3f5508e9679316aa
syleemk <syleemk@gmail.com>
2020-09-07 18:19:30 +0900

    [ADD] add CartApiController and CartService

---
+++ src/main/java/com/connect_us/backend/service/cart/CartService.java
@@ -0,0 +1,9 @@
+package com.connect_us.backend.service.cart;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CartService {
+}
---
+++ src/main/java/com/connect_us/backend/web/controller/api/v1/cart/CartApiController.java
@@ -0,0 +1,11 @@
+package com.connect_us.backend.web.controller.api.v1.cart;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/cart")
+public class CartApiController {
+}
