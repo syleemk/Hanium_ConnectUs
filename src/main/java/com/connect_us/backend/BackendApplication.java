cb9046550b80b1910a7b74db72114e23d100c20a
hanbi <rain7i@naver.com>
2020-11-16 19:56:03 +0900

    [etc] fix main

--- src/main/java/com/connect_us/backend/BackendApplication.java
+++ src/main/java/com/connect_us/backend/BackendApplication.java
@@ -29,81 +29,81 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-
-    @Bean
-    public CommandLineRunner runner(CartService cartService, FundingCartService fundingCartService, AccountRepository accountRepository, ProductRepository productRepository,
-                                    CategoryRepository categoryRepository, FundingProductRepository fundingProductRepository,FundingCartRepository fundingCartRepository ,FundingCartItemRepository fundingCartItemRepository, PasswordEncoder passwordEncoder) throws Exception {
-        return (args) -> {
-            //initialize
-            accountRepository.deleteAll();
-            productRepository.deleteAll();
-            categoryRepository.deleteAll();
-            productRepository.deleteAll();
-
-            //user, admin+password encode
-            Account user = new Account("user", passwordEncoder.encode("1234"),"user", Social.FALSE, Gender.MALE, Role.USER);
-            Account seller = new Account("seller", passwordEncoder.encode("1234"),"seller",Social.FALSE, Gender.MALE, Role.SELLER);
-            Account admin = new Account("admin", passwordEncoder.encode("1234"),"admin",Social.FALSE,Gender.MALE, Role.ADMIN);
-            Account h = new Account("hanb222@gmail.com", passwordEncoder.encode("1234"),"hanbi",Social.FALSE,Gender.FEMALE, Role.USER);
-
-            List<Account> accounts = Arrays.asList(user,seller,admin,h);
-            accountRepository.saveAll(accounts);
-
-            accountRepository.save(Account.builder()
-                    .email("syleemk@naver.com")
-                    .password("1234")
-                    .name("sylee")
-                    .role(Role.SELLER)
-                    .build()
-            );
-
-            //장바구니 생성
-            cartService.create(user);
-            cartService.create(seller);
-            cartService.create(admin);
-
-
-            //펀딩 장바구니 생성
-            fundingCartService.createFundingCart(seller.getEmail());
-            FundingCart fundingCart = fundingCartRepository.findByAccount_Email(seller.getEmail());
-
-            IntStream.rangeClosed(1, 15).forEach(index -> {
-                Category category = categoryRepository.save(Category.builder()
-                        .name("카테고리" + index)
-                        .build());
-
-                productRepository.save(Product.builder()
-                        .category(category)
-                        .account(seller)
-                        .name("상품" + index)
-                        .price(1000)
-                        .stock(40)
-                        .build());
-
-
-                FundingProduct fundingProduct = FundingProduct.builder()
-                        .category(category)
-                        .account(seller)
-                        .image("image"+index)
-                        .name("펀드상품"+index)
-                        .address("주소"+index)
-                        .goalPrice(1000)
-                        .information("정보"+index)
-                        .due(LocalDateTime.now())
-                        .build();
-
-                fundingProductRepository.save(fundingProduct);
-
-                if (index%2 == 0)
-                    fundingCartItemRepository.save(FundingCartItem.builder()
-                            .fundingCart(fundingCart)
-                            .fundingProduct(fundingProduct)
-                            .fundingInvestment(100*index)
-                            .build());
-            });
-
-        };
-    }
+//
+//    @Bean
+//    public CommandLineRunner runner(CartService cartService, FundingCartService fundingCartService, AccountRepository accountRepository, ProductRepository productRepository,
+//                                    CategoryRepository categoryRepository, FundingProductRepository fundingProductRepository,FundingCartRepository fundingCartRepository ,FundingCartItemRepository fundingCartItemRepository, PasswordEncoder passwordEncoder) throws Exception {
+//        return (args) -> {
+//            //initialize
+//            accountRepository.deleteAll();
+//            productRepository.deleteAll();
+//            categoryRepository.deleteAll();
+//            productRepository.deleteAll();
+//
+//            //user, admin+password encode
+//            Account user = new Account("user", passwordEncoder.encode("1234"),"user", Social.FALSE, Gender.MALE, Role.USER);
+//            Account seller = new Account("seller", passwordEncoder.encode("1234"),"seller",Social.FALSE, Gender.MALE, Role.SELLER);
+//            Account admin = new Account("admin", passwordEncoder.encode("1234"),"admin",Social.FALSE,Gender.MALE, Role.ADMIN);
+//            Account h = new Account("hanb222@gmail.com", passwordEncoder.encode("1234"),"hanbi",Social.FALSE,Gender.FEMALE, Role.USER);
+//
+//            List<Account> accounts = Arrays.asList(user,seller,admin,h);
+//            accountRepository.saveAll(accounts);
+//
+//            accountRepository.save(Account.builder()
+//                    .email("syleemk@naver.com")
+//                    .password("1234")
+//                    .name("sylee")
+//                    .role(Role.SELLER)
+//                    .build()
+//            );
+//
+//            //장바구니 생성
+//            cartService.create(user);
+//            cartService.create(seller);
+//            cartService.create(admin);
+//
+//
+//            //펀딩 장바구니 생성
+//            fundingCartService.createFundingCart(seller.getEmail());
+//            FundingCart fundingCart = fundingCartRepository.findByAccount_Email(seller.getEmail());
+//
+//            IntStream.rangeClosed(1, 15).forEach(index -> {
+//                Category category = categoryRepository.save(Category.builder()
+//                        .name("카테고리" + index)
+//                        .build());
+//
+//                productRepository.save(Product.builder()
+//                        .category(category)
+//                        .account(seller)
+//                        .name("상품" + index)
+//                        .price(1000)
+//                        .stock(40)
+//                        .build());
+//
+//
+//                FundingProduct fundingProduct = FundingProduct.builder()
+//                        .category(category)
+//                        .account(seller)
+//                        .image("image"+index)
+//                        .name("펀드상품"+index)
+//                        .address("주소"+index)
+//                        .goalPrice(1000)
+//                        .information("정보"+index)
+//                        .due(LocalDateTime.now())
+//                        .build();
+//
+//                fundingProductRepository.save(fundingProduct);
+//
+//                if (index%2 == 0)
+//                    fundingCartItemRepository.save(FundingCartItem.builder()
+//                            .fundingCart(fundingCart)
+//                            .fundingProduct(fundingProduct)
+//                            .fundingInvestment(100*index)
+//                            .build());
+//            });
+//
+//        };
+//    }
 
 
 }
